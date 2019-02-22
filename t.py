292e181965d09c12d44183614cb7d3a1ec0da3f3
rayrayronald <47863823+rayrayronald@users.noreply.github.com>
2019-02-22 12:35:54 +0000

    Update t.py
    indents

--- t.py
+++ t.py
@@ -33,37 +33,37 @@ while True:
         print "1"
             buf = file.read(3)
             n_file = file
-                x,y = struct.unpack( "bb", buf[1:] );
-                #print x, y
-                Long += x*X_SCALE
-                Lat += y*Y_SCALE
-                print ("Coord: x: %8f, y: %8f" % (Long, Lat));
+            x,y = struct.unpack( "bb", buf[1:] );
+            #print x, y
+            Long += x*X_SCALE
+            Lat += y*Y_SCALE
+            print ("Coord: x: %8f, y: %8f" % (Long, Lat));
+            break
+    if button.is_pressed:
+        print ("LOADING DATABASE...")
+        URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?radius="+str(Radius)+"&key=AIzaSyA3aYU6UKfZkp8QfafB2WCfouPjxVrFx2A&location="+str(Lat)+","+str(Long)
+        print URL
+        html=urllib.urlopen(URL)
+        htmltext=html.read()
+        #htmltext = "TEST\"name\" : \"Imperial College London\",TESTTEST\"name\" : \"Museum\",TESTTEST\"name\" : \"NATAAAA\",TEST"
+        print("DATABASE UPDATED!\n")
+        postname = 1
+        for i in range(NUMBER_READOUTS+1):
+            phrase =  "\"name\" : \""
+            prename = htmltext.find(phrase,postname)
+            postname =  htmltext.find("\"", prename+len(phrase)+1)
+            name[i] = htmltext[prename+len(phrase):postname]
+            if name[i] == "l_attributions":
+                os.system('espeak "There are no more places nearby" 2>/dev/null')
                 break
-        if button.is_pressed:
-            print ("LOADING DATABASE...")
-            URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?radius="+str(Radius)+"&key=AIzaSyA3aYU6UKfZkp8QfafB2WCfouPjxVrFx2A&location="+str(Lat)+","+str(Long)
-            print URL
-                html=urllib.urlopen(URL)
-                htmltext=html.read()
-                #htmltext = "TEST\"name\" : \"Imperial College London\",TESTTEST\"name\" : \"Museum\",TESTTEST\"name\" : \"NATAAAA\",TEST"
-                print("DATABASE UPDATED!\n")
-                postname = 1
-                for i in range(NUMBER_READOUTS+1):
-                    phrase =  "\"name\" : \""
-                        prename = htmltext.find(phrase,postname)
-                        postname =  htmltext.find("\"", prename+len(phrase)+1)
-                        name[i] = htmltext[prename+len(phrase):postname]
-                        if name[i] == "l_attributions":
-                            os.system('espeak "There are no more places nearby" 2>/dev/null')
-                            break
-                        if i == 0:
-                            start_flag = prename
-                                os.system('espeak "{0}, Top {1} places within {2} metres are" 2>/dev/null'.format(name[i],NUMBER_READOUTS,Radius))
-                        else:
-                            print(i),
-                            print(": "),
-                            print(name[i])
-                            os.system('espeak "{0}" 2>/dev/null'.format(name[i]))
+            if i == 0:
+                start_flag = prename
+                    os.system('espeak "{0}, Top {1} places within {2} metres are" 2>/dev/null'.format(name[i],NUMBER_READOUTS,Radius))
+            else:
+                print(i),
+                print(": "),
+                print(name[i])
+                os.system('espeak "{0}" 2>/dev/null'.format(name[i]))
 if exit.is_pressed:
     print ("Bye~")
     print ("IP Adress for SSH:")
