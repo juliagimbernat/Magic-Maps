449a2b56ca1cf4ccc8fc8ff03ffdd0bb8988c852
rayrayronald <47863823+rayrayronald@users.noreply.github.com>
2019-03-20 07:09:34 +0000

    write

--- Write.py
+++ Write.py
@@ -1,8 +1,12 @@
 import MFRC522
 START_X = str(-0.1736854)
-START_Y = str(51.4966478)
+START_Y_I = 51.4966478
 END_X = str(-0.1640704)
-END_Y = str(51.5006107)
+END_Y_I = 51.5006107
+
+
+START_Y = str(START_Y_I)
+END_Y = str((END_Y_I-START_Y_I)*0.8+START_Y_I)
 
 
 text = START_X+","+START_Y+","+END_X+","+END_Y+","
@@ -28,7 +32,26 @@ while True:
             i += 1
         MIFAREReader.MFRC522_StopCrypto1()
         break
-
+while True:
+    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+    if status == MIFAREReader.MI_OK:
+        print "Card detected"
+        (status, uid) = MIFAREReader.MFRC522_Anticoll()
+        MIFAREReader.MFRC522_SelectTag(uid)
+        status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 11, KEY, uid)
+    data = []
+    text_read = ''
+    if status == MIFAREReader.MI_OK:
+        for block_num in BLOCK_ADDRS:
+            block = MIFAREReader.MFRC522_Read(block_num)
+            if block:
+                data += block
+        if data:
+            text_read = ''.join(chr(i) for i in data)
+        MIFAREReader.MFRC522_StopCrypto1()
+        print "Written"
+        print text_read
+        break
 
 
 
